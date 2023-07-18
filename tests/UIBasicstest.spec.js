64e6d3ea28644e798fb1f8f73a67fccc469f44f4
Andrew Dinh <andrewdinh74@yahoo.com>
2023-07-18 07:19:10 -0700

    Add example without browser context

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,9 +1,13 @@
-const { test } = require('@playwright/test');
+const { test } = require("@playwright/test");
 
-test('First test with browser context', async ({ browser }) => {
+test("First test with browser context", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  page.goto('https://google.com');
+  page.goto("https://google.com");
+});
 
+// If you just want default page without browser context
+test("First test without browser context", async ({ page }) => {
+  page.goto("https://google.com");
 });
