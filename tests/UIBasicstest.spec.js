4133ad270107120b2aa26611e32af670cbccd2b8
Andrew Dinh <andrewdinh74@yahoo.com>
2023-07-18 07:56:10 -0700

    Finish first test

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,13 +1,14 @@
-const { test } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
 test("First test with browser context", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  page.goto("https://google.com");
+  await page.goto("https://google.com");
+  await expect(page).toHaveTitle("Google");
 });
 
 // If you just want default page without browser context
-test("First test without browser context", async ({ page }) => {
-  page.goto("https://google.com");
-});
+// test("First test without browser context", async ({ page }) => {
+//   await page.goto("https://google.com");
+// });
